95f8970cfbbf115a2111ea975d3b6d870fa97dae
Francis Rodrigues <dev@tosystems.net>
2017-01-13 18:35:07 -0200

    ENH: Added an ArrayAdapter to PlaceholderFragment in Main activity

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -53,6 +54,8 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        ArrayAdapter<String> mForecastAdapter;
+
         public PlaceholderFragment() {
         }
 
@@ -71,6 +74,15 @@ public class MainActivity extends ActionBarActivity {
                     "Sun 6/29 - Sunny 20/7"
             };
             List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
+
+            // Now that we have some dummy forecast data, create an ArrayAdapter.
+            // The ArrayAdapter will take data from a source (like our dummy forecast) and
+            // use it to populate the ListView it's attached to.
+            mForecastAdapter = new ArrayAdapter<>(
+                    getActivity(), // The current context (this activity).
+                    R.layout.list_item_forecast, // The name of the layout ID.
+                    R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                    weekForecast); // The String array of data.
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
