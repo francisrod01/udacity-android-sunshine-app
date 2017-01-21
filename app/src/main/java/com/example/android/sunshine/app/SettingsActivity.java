f8853de69eef6bc1170f31626d6f9e810a55b741
Francis Rodrigues <dev@tosystems.net>
2017-01-20 23:22:56 -0200

    Add units setting

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -31,6 +31,8 @@ public class SettingsActivity extends PreferenceActivity
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes
         bindPreferenceSymmaryToValue(findPreference(getString(R.string.pref_location_key)));
+
+        bindPreferenceSymmaryToValue(findPreference(getString(R.string.pref_units_key)));
     }
 
     /**
