44cbc9d00966fabd17f1a966f35a4587f8b822ee
Francis Rodrigues <dev@tosystems.net>
2017-03-11 21:59:38 -0300

    4.09 - Writed uri matcher

--- app/src/androidTest/java/com/example/android/sunshine/app/data/TestUriMatcher.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/data/TestUriMatcher.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app.data;
 
+import android.content.UriMatcher;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
@@ -15,7 +16,7 @@ import com.example.android.sunshine.app.data.WeatherContract.WeatherEntry;
  * the test must be in the same data package as the Android app code. Doing the test this way is
  * a nice compromise between data hiding and testability.
  */
-public class TestUrlMatcher extends AndroidTestCase {
+public class TestUriMatcher extends AndroidTestCase {
     private static final String LOCATION_QUERY = "London, UK";
     private static final long TEST_DATE = 1419033600L; // December 20th, 2014
     private static final long TEST_LOCATION_ID = 10L;
@@ -35,21 +36,21 @@ public class TestUrlMatcher extends AndroidTestCase {
        for each of the Uri types that our ContentProvider can handle. Uncomment this when you are
        ready to test your UriMatcher.
      */
-//    public void testUriMatcher() {
-//        UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
-//
-//        assertEquals("Error: The WEATHER URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_DIR), WeatherProvider.WEATHER);
-//
-//        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR),
-//                WeatherProvider.WEATHER_WITH_LOCATION);
-//
-//        assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR),
-//                WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE);
-//
-//        assertEquals("Error: The LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_LOCATION_DIR), WeatherProvider.LOCATION);
-//    }
+    public void testUriMatcher() {
+        UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
+
+        assertEquals("Error: The WEATHER URI was matched incorrectly.",
+                testMatcher.match(TEST_WEATHER_DIR), WeatherProvider.WEATHER);
+
+        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
+                testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR),
+                WeatherProvider.WEATHER_WITH_LOCATION);
+
+        assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
+                testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR),
+                WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE);
+
+        assertEquals("Error: The LOCATION URI was matched incorrectly.",
+                testMatcher.match(TEST_LOCATION_DIR), WeatherProvider.LOCATION);
+    }
 }
--- app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
@@ -97,20 +97,28 @@ public class WeatherProvider extends ContentProvider {
     }
 
     /*
-       Students: Here ie where you ned to create the UriMatcher. This UriMatcher will
+       Students: Here is where you need to create the UriMatcher. This UriMatcher will
        match each URI to the WEATHER, WEATHER_WITH_LOCATION, WEATHER_WITH_LOCATION_AND_DATE,
        and LOCATION integer constants defined above. You can test this by uncommenting the
        testUriMatcher test within TestUriMatcher.
      */
     static UriMatcher buildUriMatcher() {
-        // 1) The code passed into the constructor represents the code to return for the root
-        // URI. It's common NO_MATCH as the code for this case. Add the constructor below.
+        // I know what you're thinking. Why create a UriMatcher when you can use regular
+        // expressions instead? Because you're not crazy, that's why.
 
-        // 2) Use the addURI function to match each of the types. Use the constants from
-        // WeatherContract to help define the types to the UriMatcher.
+        // All paths added to the UriMatcher have a corresponding code to return when a match is
+        // found. The code passed into the constructor represents the code to return for the root
+        // URI. It's common to use NO_MATCH as the code for this case.
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
 
-        // 3) Return the new matcher!
-        return null;
+        // For each type of URI you want to add, create a corresponding code.
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
+        return matcher;
     }
 
     /*
